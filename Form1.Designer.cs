3684f71a9da4b963817ecd348b8184859b5034bc
Astra782 <rjorellanap@unah.hn>
2025-04-20 23:02:22 -0600

    Se añadio imagen de la empresa

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.EtiquetaEncabezado = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel6 = new System.Windows.Forms.Panel();
             this.btn_ajustes = new System.Windows.Forms.Button();
@@ -39,35 +39,24 @@
             this.btn_registro = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.btn_consultas = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel6.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.EtiquetaEncabezado);
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(444, 56);
+            this.panel1.Size = new System.Drawing.Size(444, 204);
             this.panel1.TabIndex = 0;
-            // 
-            // EtiquetaEncabezado
-            // 
-            this.EtiquetaEncabezado.AutoSize = true;
-            this.EtiquetaEncabezado.Dock = System.Windows.Forms.DockStyle.Top;
-            this.EtiquetaEncabezado.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EtiquetaEncabezado.Location = new System.Drawing.Point(0, 0);
-            this.EtiquetaEncabezado.Name = "EtiquetaEncabezado";
-            this.EtiquetaEncabezado.Size = new System.Drawing.Size(207, 25);
-            this.EtiquetaEncabezado.TabIndex = 0;
-            this.EtiquetaEncabezado.Text = "RinTinTin Vet Center";
-            this.EtiquetaEncabezado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.EtiquetaEncabezado.Click += new System.EventHandler(this.label1_Click);
             // 
             // panel2
             // 
@@ -76,7 +65,7 @@
             this.panel2.Controls.Add(this.panel4);
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 62);
+            this.panel2.Location = new System.Drawing.Point(0, 194);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(444, 344);
             this.panel2.TabIndex = 1;
@@ -167,23 +156,34 @@
             this.btn_consultas.Click += new System.EventHandler(this.button1_Click);
             this.btn_consultas.MouseHover += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(117, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 182);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PowderBlue;
-            this.ClientSize = new System.Drawing.Size(444, 406);
+            this.ClientSize = new System.Drawing.Size(444, 538);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "RinTinTin Vet Center";
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel6.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -193,7 +193,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Label EtiquetaEncabezado;
         private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Panel panel4;
@@ -201,6 +200,7 @@
         private System.Windows.Forms.Button btn_report;
         private System.Windows.Forms.Button btn_registro;
         private System.Windows.Forms.Button btn_consultas;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -26,5 +26,10 @@ namespace VeterinariaRintintin
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
